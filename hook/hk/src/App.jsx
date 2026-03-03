5d2eb682dbcf4ab03801dd65df11e9ee89a16694
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-03-03 15:21:39 +0530

    Brushed up the concepts of Hooks in react

--- hook/hk/src/App.jsx
+++ hook/hk/src/App.jsx
@@ -1,39 +1,65 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
 
+// functio n App() {
+
+//   let [counter, setCounter] = useState(0);
+
+//   function addValue(){
+//     if(counter===10){
+//       return;
+//     }
+//     setCounter(counter+1);
+    
+//   }
+
+//    const subValue=()=>{
+//     if(counter===0){
+//       return;
+//     }
+//     setCounter(counter-1);
+//   }
+//   return (
+//     <>
+//     <h2>Value is : {counter} </h2>
+    
+//      <button 
+//      onClick={addValue}
+//      >Increment</button>
+//      <br></br>
+//     <button 
+//     onClick={subValue}
+//     >Decrement</button>
+//     </>
+//   )
+// }
+
+function App1(){
   let [counter, setCounter] = useState(0);
 
-  function addValue(){
-    if(counter===10){
-      return;
-    }
+  const addV = ()=> {
     setCounter(counter+1);
     
   }
 
-   const subValue=()=>{
-    if(counter===0){
-      return;
-    }
+  const decV =()=>{ 
     setCounter(counter-1);
-  }
-  return (
-    <>
-    <h2>Value is : {counter} </h2>
+
     
-     <button 
-     onClick={addValue}
-     >Increment</button>
-     <br></br>
-    <button 
-    onClick={subValue}
-    >Decrement</button>
-    </>
+  }
+  return(
+    <>
+  <h1>Hello</h1>
+  <button onClick={addV}>Increment {counter}</button>
+  <button onClick={decV}> Decrement {counter}</button>
+  </>
   )
 }
 
-export default App
+
+
+
+
+export default App1;
--- hook/hk/src/main.jsx
+++ hook/hk/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App1 from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    <App1 />
+
 )
