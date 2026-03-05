eac2c070e1e59df26e2bc2823ddd974a09b8e276
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-03-05 10:03:53 +0530

    Understood more about importing and reusing ready made cards

--- tprop/twindProp/src/App.jsx
+++ tprop/twindProp/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Cards'
+import Card3 from './components/Mcard'
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
   <h1 className='bg-green-400 text-black'> Hello</h1>
-    </>
+
+    <Card />
+  <Card3 />
+   </>
+   
   )
 }
-
 export default App
